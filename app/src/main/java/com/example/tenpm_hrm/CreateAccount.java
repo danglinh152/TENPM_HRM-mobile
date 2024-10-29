9a7c90b345276b68d0515d02cb6e7a522cad8f08
Toanttt <22521490@gm.uit.edu.vn>
2024-10-30 00:14:09 +0700

    git add -f, push lai

---
+++ app/src/main/java/com/example/tenpm_hrm/CreateAccount.java
@@ -0,0 +1,12 @@
+package com.example.tenpm_hrm;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccount extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+    }
+}
