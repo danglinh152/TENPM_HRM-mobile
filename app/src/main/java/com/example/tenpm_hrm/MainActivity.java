a699640244d07a1a2499c0f5a9ecc44bcb9e8a84
Nguyen Van Hoa <ngvanhoa130504@gmail.com>
2024-12-04 09:49:02 +0700

    database

--- app/src/main/java/com/example/tenpm_hrm/MainActivity.java
+++ app/src/main/java/com/example/tenpm_hrm/MainActivity.java
@@ -6,12 +6,13 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DatabaseHandler dbHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login);
-
+        dbHandler = new DatabaseHandler(this);
+        dbHandler.addDe();
     }
 }
